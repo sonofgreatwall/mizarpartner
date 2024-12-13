db33fd079faa6330c4f1a92e98466f6cb40629ce
Vitalii Dendura <unclescar0813@gmail.com>
2024-12-13 21:35:37 +0900

    update

--- assets/js/js.js
+++ assets/js/js.js
@@ -24,3 +24,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+function openMenu(x) {
+  x.classList.toggle("change");
+}
